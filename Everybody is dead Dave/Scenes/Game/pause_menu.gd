ce8cade1b4683c6608fb423ce4a10cc15ef29b3c
AKHarrison <al.harrison@gmail.com>
2026-04-02 12:10:10 +0100

    new map stuff

--- Everybody is dead Dave/Scenes/Game/pause_menu.gd
+++ Everybody is dead Dave/Scenes/Game/pause_menu.gd
@@ -4,10 +4,17 @@ func _ready():
 	hide()
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	
-func _input(event: InputEvent):
-	if event.is_action_pressed("ui_cancel"):
-		toggle_pause()
 
+func _can_pause() -> bool:
+	var scene = get_tree().current_scene
+	if scene == null:
+		return false
+	return scene.scene_file_path != "res://Scenes/Game/main_menu.tscn"
+
+func _input(event: InputEvent):
+	if event.is_action_pressed("ui_cancel") and _can_pause():
+		toggle_pause()
+		
 func toggle_pause():
 	var is_paused = not get_tree().paused
 	get_tree().paused = is_paused
