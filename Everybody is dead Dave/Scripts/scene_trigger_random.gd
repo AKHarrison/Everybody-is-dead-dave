9ccce0342787bd22787468db83add04063aa24bc
AKHarrison <al.harrison@gmail.com>
2026-04-15 22:14:47 +0100

    random level select

---
+++ Everybody is dead Dave/Scripts/scene_trigger_random.gd
@@ -0,0 +1,20 @@
+class_name SceneTriggerRandom extends Area2D
+
+var scene_folder = "res://Scenes/Game/"
+
+var levels = [
+	"nice_terrace_gauntlet",
+	"nice_terrace_gauntlet_flip",
+	"ruff_terrace",
+	]
+	
+
+func _on_body_entered(body):
+	if body is Player:
+		select_level()
+		
+	
+	
+func select_level():
+	var connected_scene = levels.pick_random()
+	scene_manager.change_scene(get_owner(), connected_scene)
