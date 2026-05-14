1fbd127840e51528f68cd8268c75d49d50b1277d
AKHarrison <al.harrison@gmail.com>
2026-05-15 00:07:31 +0100

    finished bridge and added physics layer tileset

--- Everybody is dead Dave/Scenes/Game/bridge.gd
+++ Everybody is dead Dave/Scenes/Game/bridge.gd
@@ -7,11 +7,3 @@ func _on_body_entered(body: Node2D) -> void:
 		body.set_collision_layer_value(2, true)
 		body.set_collision_mask_value(1, false)
 		body.set_collision_layer_value(1, false)
-
-func _on_body_exited(body: Node2D) -> void:
-	if body is Player:
-		body.z_index = 0
-		body.set_collision_mask_value(2, false)
-		body.set_collision_layer_value(2, false)
-		body.set_collision_mask_value(1, true)
-		body.set_collision_layer_value(1, true)
--- Everybody is dead Dave/Scenes/Game/bridge_floor_exit.gd
+++ Everybody is dead Dave/Scenes/Game/bridge_floor_exit.gd
@@ -1,20 +1,10 @@
 extends Area2D
 
-var body_z_index = 1
 
-func exit_on_body_entered(body: Node2D) -> void:
+func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
-		if body.z_index == body_z_index:
-			body.z_index = 0
-			body.set_collision_mask_value(1, true)
-			body.set_collision_layer_value(1, true)
-			body.set_collision_mask_value(5, false)
-			body.set_collision_layer_value(5, false)
-
-func exit_on_body_exited(body: Node2D) -> void:
-	if body is Player:
-		body.z_index = 1
-		body.set_collision_layer_value(1, false)
-		body.set_collision_mask_value(1, false)
-		body.set_collision_mask_value(5, true)
-		body.set_collision_layer_value(5, true)
+		body.z_index = 0
+		body.set_collision_mask_value(2, false)
+		body.set_collision_layer_value(2, false)
+		body.set_collision_mask_value(1, true)
+		body.set_collision_layer_value(1, true)
