3697b35a53099113d460cdbc045a0cf2fd8d7533
AKHarrison <al.harrison@gmail.com>
2026-05-17 11:44:12 +0100

    fixd all spawn bugs

--- Everybody is dead Dave/Scripts/dual_carriageway.gd
+++ Everybody is dead Dave/Scripts/dual_carriageway.gd
@@ -8,21 +8,18 @@ const CAR_TYPES = [
 ]
 	
 
-func spawn_car(spawn_point: Marker2D, car_pool: Array, delay: float, velocity: float, direction: Vector2 = Vector2.LEFT):
+func spawn_car(spawn_point: Marker2D, car_pool: Array, delay: float, velocity: float, direction: Vector2 = Vector2.LEFT, flip: bool = false):
 	await get_tree().create_timer(delay).timeout
 	var new_car = car_pool.pick_random().instantiate()
 	new_car.global_position = spawn_point.global_position
 	new_car.direction = direction
 	new_car.speed = velocity
 	add_child(new_car)
-	new_car.flip_sprite()
+	if flip:
+		new_car.flip_sprite()
 
 func _on_timer_timeout():
 	spawn_car(%CarSpawn1, CAR_TYPES, randf_range(0.05, 0.4), 300, Vector2.LEFT)
 	spawn_car(%CarSpawn2, CAR_TYPES, randf_range(0.05, 0.4), 500, Vector2.LEFT)
-	spawn_car(%CarSpawn3, CAR_TYPES, randf_range(0.05, 0.4), 500, Vector2.RIGHT)
-	spawn_car(%CarSpawn4, CAR_TYPES, randf_range(0.05, 0.4), 300, Vector2.RIGHT)
-
-
-func _on_bridge_floor_exit_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
+	spawn_car(%CarSpawn3, CAR_TYPES, randf_range(0.05, 0.4), 500, Vector2.RIGHT, true)
+	spawn_car(%CarSpawn4, CAR_TYPES, randf_range(0.05, 0.4), 300, Vector2.RIGHT, true)
